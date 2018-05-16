198bf0ef9a91436efdf0993704c4524077b0f70f
Meg MacIver <megmaciver13@gmail.com>
2018-05-16 15:47:25 -0400

    build Header and MapContainer components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Header here!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/MapContainer/MapContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class MapContainer extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Map Container here!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default MapContainer
