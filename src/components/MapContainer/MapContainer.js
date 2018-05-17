a69b4fa73275289e7ba87aa7570c870d3dc0721d
Meg MacIver <megmaciver13@gmail.com>
2018-05-16 21:31:54 -0400

    attempt to add map view

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -9,6 +9,7 @@ class MapContainer extends Component {
         return(
             <div>
                 <h1>Map Container here!!!</h1>
+                <div id="viewDiv"></div>
             </div>
         )
     }
